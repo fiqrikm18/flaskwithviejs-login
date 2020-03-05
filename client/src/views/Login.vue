639e6d6067f927091051fa68be27f57394a19a14
fiqri khoirul muttaqin <fiqrikm18@gmail.com>
2020-03-05 17:38:47 +0700

    add login page

---
+++ client/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div>
+            <form class="form">
+                <h2>User Login</h2>
+                <div class="input-container">
+                    <i class="fa fa-user icon"></i>
+                    <input type="text" class="input-field" placeholder="Username"/>
+                </div>
+
+                <div class="input-container">
+                    <i class="fa fa-key icon"></i>
+                    <input type="text" class="input-field" placeholder="Password"/>
+                </div>
+
+                <button class="btn">Login</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
